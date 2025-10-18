f811b38f2425abd745a99be3aafcf8f75f8aacd0
SacrumImp <shishov265@gmail.com>
2025-10-18 16:54:20 +0300

    added vacancy card

--- vector-ui/src/pages/hr.tsx
+++ vector-ui/src/pages/hr.tsx
@@ -1,10 +1,20 @@
+import { VacancyCard } from "@/components/vacancy-card/vacancy-card";
 import { DefaultLayout } from "@/layouts/default";
 
 export default function HRPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        HR
+        <VacancyCard
+            id={"1"}
+            title={"efeff"}
+            company="fefwefwe"
+            postedDate="2024-12-13"
+            daysLeft={3}
+            applicants={3}
+            hasNewApplicants
+            status="active"
+        />
       </section>
     </DefaultLayout>
   );
